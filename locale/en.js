92b480f5ac80c9ab155d3ddca634944594aa3132
Andrey Voronin <AVor0n@yandex.ru>
2023-05-13 14:56:52 +0500

    feat(app): localization

--- locale/en.js
+++ locale/en.js
@@ -1,34 +1,114 @@
 export const en = {
-    leads:{
-        title: "The most active dreamers",
-        more: 'Show all dreams',
-        dreamCard:{
-            title: "",
-            text: "",
-            lastUpdate: "Last transfer {0} ago",
-            progress: {
-                current: "€ {0} collected",
-                total: "from € {0}",
-            }
-        }
+    header: {
+        title: '€ 355 320 000',
+        text: ['Built on the nacometa.com platform', 'Share your dream and get community support!'],
+        startBtn: 'Start fundraising',
+        videoBtn: 'Watch video',
     },
-    slogan: "",
+    nav: {
+        links: ['How it works', 'Success stories', 'Opportunities', 'FAQ', 'Contacts'],
+    },
+    leads: {
+        title: 'The most active dreamers',
+        more: 'Show all dreams',
+        dreamCard: {
+            title: '',
+            text: '',
+            lastUpdate: 'Last update {0} ago',
+            progress: {
+                current: '€ {0} raised',
+                total: 'out of € {0}',
+            },
+        },
+    },
+    slogan: 'Naсometa.com - an online community of voluntary financial assistance',
     howTo: {
-        title: "",
+        title: "Support community members' dreams for just € 100 and raise up to € 5,000 for each of your own dreams!",
         stats: {
-            title: "",
-            amount: "",
-            text: "",
+            title: 'You can raise up to:',
+            amount: '€ 5,000',
+            text: 'According to our statistics, this amount can be realistically raised within 30 days',
         },
         instructions: {
-            title: "",
+            title: 'How it works',
             steps: [
-                '',
-                ''
+                'Tell us about your dream',
+                'Support the dreams of four members with € 10, € 20, € 30, and € 40',
+                'Subscribe to access your campaign statistics',
+                'Share your dreams with the world - receive unlimited support from friends and Nacometa community members',
             ],
-            link: '',
-            start: '',
-            more: ''
+            link: 'here',
+            start: 'Start fundraising',
+            more: 'Learn more',
         },
     },
-}
+    feedback: {
+        title: 'These people have already fulfilled their dreams',
+    },
+    opportunities: {
+        title: 'Opportunities',
+        text: 'An easy, fast, and effective way to make your dream come true!',
+        cards: [
+            {
+                title: 'Free',
+                text: "An absolutely transparent non-commercial platform without exchange fees. We do not accumulate participants' funds.",
+            },
+            {
+                title: 'Without intermediaries',
+                text: 'There are no third parties or intermediaries between the participants. Only direct exchange of funds.',
+            },
+            {
+                title: 'Easy',
+                text: 'Track the growth of your team and incoming transfers using a convenient platform.',
+            },
+            {
+                title: 'Convenient and secure',
+                text: 'Multilingual interface and a secure system with round-the-clock technical support.',
+            },
+        ],
+    },
+    createDream: {
+        title: 'Tell us about your dream right now!',
+        uploadFileLabel: {
+            desktop: ['Drag and drop an image here or', 'upload from your computer'],
+            mobile: 'Click to attach an image',
+        },
+        target: 'Fundraising goal',
+        submitBtn: 'Continue',
+        progress: {
+            title: 'Here will be the name of your dream',
+            current: '€ {0} raised',
+            total: ' out of € {0}',
+        },
+    },
+    faq: {
+        title: 'Frequently asked questions',
+        items: [
+            {
+                question: 'What is a fundraising campaign?',
+                answer: 'Fundraising is the process of seeking and attracting financial resources from individuals, funds, or companies to implement socially significant projects. Fundraising campaigns can be aimed at various goals, from helping students pay for their education to fulfilling someones dream of traveling to space. The goal of the nacometa.com community is to develop a platform for mutually beneficial fundraising, which will make it easy and fast to raise funds to fulfill your dream.',
+            },
+            {
+                question: 'How do I start and raise funds in Nacometa?',
+                answer: 'To create a campaign on the nakometa.com platform, you need to register and fill out the campaign creation form. Then you should describe your dream, fundraising goal, and deadlines for raising the necessary amount. After that, the campaign is published on the platform and becomes available to all participants.',
+            },
+            {
+                question: 'Is my fundraising in Nacometa limited by amount and time?',
+                answer: 'There is a limit on the maximum amount and time for fundraising in Nacometa. Each project has its own goal and deadline for raising funds. The maximum amount that can be raised also depends on the type of project and the country where it is located. Usually, Nacometa sets a limit on fundraising in the amount of 1 million US dollars, and the maximum fundraising period is 90 days. However, it is possible to extend the fundraising period if necessary, after coordination with the platform team.',
+            },
+            {
+                question: 'How can I track the results of my fundraising campaign?',
+                answer: 'The nakometa.com platform provides a convenient and transparent system for tracking the progress of your campaign, as well as donations from participants. You can easily manage your campaign, track the number of donations received, and comments from participants. Also, you will receive notifications every time the campaign receives a new donation.',
+            },
+            {
+                question: 'Is it legal to raise funds in this way and do I need to pay taxes?',
+                answer: 'Yes, fundraising through the Nacometa platform is legal. However, it depends on the goals of the person raising funds. If the fundraising is aimed at creating a project or business, then tax obligations must be taken into account. In most countries, there are tax rules that regulate the payment of taxes on profits earned. Therefore, if you are raising funds to create a business or project, you need to find out what tax obligations exist in your country and what needs to be done. In any case, it is recommended to consult a lawyer or tax consultant to obtain specific information about tax obligations and special requirements in your country.',
+            },
+        ],
+    },
+    footer: {
+        title: ['Have questions? ', 'We are open for communication and support 24/7'],
+        docs: ['Terms of use', 'Privacy policy'],
+        copyright: '© 2021 Nacometa. All rights reserved',
+    },
+};
