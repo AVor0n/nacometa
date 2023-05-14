d70e878829aa4e3b95635f9c22da3ac85b6ffcb9
Andrey Voronin <AVor0n@yandex.ru>
2023-05-14 23:19:05 +0500

    fix(video): cant vertical scroll

--- js/controllers/video.js
+++ js/controllers/video.js
@@ -6,14 +6,21 @@ export const init = () => {
     const prevBtn = slider.querySelector('.video-slider__prev-btn');
     const scrollStep = card.clientWidth / 2;
 
+    const isEnd = () => cardsContainer.scrollLeft + cardsContainer.clientWidth + 10 >= cardsContainer.scrollWidth;
+    const isStart = () => cardsContainer.scrollLeft === 0;
+
     const updateBtns = () => {
-        nextBtn.disabled = cardsContainer.scrollLeft + cardsContainer.clientWidth === cardsContainer.scrollWidth;
-        prevBtn.disabled = cardsContainer.scrollLeft === 0;
+        nextBtn.disabled = isEnd();
+        prevBtn.disabled = isStart();
     };
 
     updateBtns();
 
     cardsContainer.addEventListener('wheel', e => {
+        const direction = e.deltaY < 0 ? 'up' : 'down';
+        if ((direction === 'up' && isStart()) || (direction === 'down' && isEnd())) {
+            return;
+        }
         cardsContainer.scrollBy(e.deltaY, 0);
         updateBtns();
         e.preventDefault();
